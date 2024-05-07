27227216abb436b5f2b9bec20d147ce7232e11d2
Djood <github.h1m5z@aleeas.com>
2024-05-07 18:35:49 +0200

    download and run the utils file

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Structure du projet
 
-- [ ] download and run the setup file"
+- [x] download and run the setup file"
+
+```bash
+bash -c "$(curl -sL https://raw.githubusercontent.com/djoudi-cherfi/setup-test/main/src/os/setup.sh)"
+```
+
+- [ ] download and run the utils file"
--- src/os/setup.sh
+++ src/os/setup.sh
@@ -1,1 +1,58 @@
+#!/usr/bin/env bash
+
 echo "download and run the setup file"
+
+declare -r GITHUB_REPOSITORY="djoudi-cherfi/setup-test"
+
+declare -r DOTFILES_ORIGIN="git@github.com:$GITHUB_REPOSITORY.git"
+declare -r DOTFILES_TARBALL_URL="https://github.com/$GITHUB_REPOSITORY/tarball/main"
+declare -r DOTFILES_UTILS_URL="https://raw.githubusercontent.com/$GITHUB_REPOSITORY/main/src/os/utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+download() {
+
+    local url="$1"
+    local output="$2"
+
+    if command -v "curl" &> /dev/null; then
+
+        curl --location --silent --show-error --output "$output" "$url" &> /dev/null
+        return $?
+
+    elif command -v "wget" &> /dev/null; then
+
+        wget --quiet --output-document="$output" "$url" &> /dev/null
+        return $?
+    fi
+
+    return 1
+}
+
+download_utils() {
+
+    local tmpFile=""
+
+    tmpFile="$(mktemp /tmp/XXXXX)"
+
+    download "$DOTFILES_UTILS_URL" "$tmpFile" \
+        && . "$tmpFile" \
+        && rm -rf "$tmpFile" \
+        && return 0
+
+   return 1
+}
+
+# ----------------------------------------------------------------------
+# | Main                                                               |
+# ----------------------------------------------------------------------
+
+main() {
+
+    # Load utils
+    if [ -x "utils.sh" ]; then
+        . "utils.sh" || exit 1
+    else
+        download_utils || exit 1
+    fi
+}
