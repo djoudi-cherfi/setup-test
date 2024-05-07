3b41fd1f8bb5d697b2356225149a38d6c6ab1652
djoudi-cherfi <github.h1m5z@aleeas.com>
2024-05-07 19:05:23 +0200

    ask to run sudo

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Structure du projet
 
-- [x] download and run the setup file"
+- [x] download and run the setup file
 
 ```bash
 bash -c "$(curl -sL https://raw.githubusercontent.com/djoudi-cherfi/setup-test/main/src/os/setup.sh)"
 ```
 
-- [ ] download and run the utils file"
+- [x] download and run the utils file
+
+- [ ] ask to run sudo
--- src/os/utils.sh
+++ src/os/utils.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
 echo "download and run the utils file"
+
+echo "ask to run sudo"
+
+ask_for_sudo() {
+
+    # Ask for the administrator password upfront.
+    sudo -v &> /dev/null
+
+    # Update existing `sudo` time stamp
+    # until this script has finished.
+
+    while true; do
+        sudo -n true
+        sleep 60
+        kill -0 "$$" || exit
+    done &> /dev/null &
+}
