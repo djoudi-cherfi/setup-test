5d38a31e013c5234ff2c2b1823db730fe2352a28
djoudi-cherfi <github.h1m5z@aleeas.com>
2024-05-15 16:04:07 +0200

    Installs tools xcode

---
+++ src/os/installs/main.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+echo -e "\n• Installs"
+
+"./$(get_os)/main.sh"
